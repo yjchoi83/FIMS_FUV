20d269c488af2b266f2def2305ec546152c03b42
yjchoi83 <choiyj83@gmail.com>
2020-11-14 13:18:02 +0900

    Update FIMS_data_preprocess.py

--- FIMS_data_preprocess.py
+++ FIMS_data_preprocess.py
@@ -4,103 +4,77 @@ import pandas as pd
 import sys,csv
 import datetime
 
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.preprocessing import StandardScaler
-from sklearn.preprocessing import Normalizer
-from sklearn.preprocessing import MaxAbsScaler
-
-from keras.layers import Dense, Dropout, Activation,LeakyReLU,PReLU
-from sklearn import preprocessing
-from keras.models import Sequential
-from keras.optimizers import SGD,Adam,RMSprop
-from keras.utils import np_utils
-from keras import initializers
-
-from scipy.optimize import curve_fit
-from keras import metrics
-from sklearn.model_selection import train_test_split
-
-import matplotlib.pyplot as plt
-from math import  log
-
-dt = datetime.datetime.now()
-dt = dt.strftime("%m%d%H%M%S")
-
-##### 1. loading files #####
-# training data loading
-data_set_train = pd.read_csv('./dataset/train_log.csv',header=None)
+##### reading allsky file #####
+data_set_train = pd.read_csv('./dataset/allskymap_raw.csv',header=None)
 data_set_train = pd.DataFrame(data_set_train)
-data_set_train = data_set_train.values[:]
+data_set_train = data_set_train.values[1:]
 
-# test data loading (log scale)
-data_set_test = pd.read_csv('./dataset/test_log_all.csv',header=None)
-data_set_test = pd.DataFrame(data_set_test)
-data_set_test = data_set_test.values[:]
+index = data_set_train[:,0]
+l = data_set_train[:,1]
+b = data_set_train[:,2]
+non_fims = data_set_train[:,3]
+non_galex =data_set_train[:,4]
+non_rass1 =data_set_train[:,5]
+non_rass2 = data_set_train[:,6]
+non_ha = data_set_train[:,7]
+non_ebv = data_set_train[:,8]
+non_hi = data_set_train[:,9]
 
-# test data loading (raw)
-data_set_test_raw = pd.read_csv('./dataset/test_raw_all.csv',header=None)
-data_set_test_raw = pd.DataFrame(data_set_test_raw)
-data_set_test_raw = data_set_test_raw.values[:]
+l = l.astype('float32')
+b = b.astype('float32')
+non_fims = non_fims.astype('float32')
+non_galex = non_galex.astype('float32')
+non_rass1 = non_rass1.astype('float32')
+non_rass2 = non_rass2.astype('float32')
+non_ha = non_ha.astype('float32')
+non_ebv = non_ebv.astype('float32')
+non_hi = non_hi.astype('float32')
 
-##### 2. data set #####
-all_train = data_set_train[:,1:9]
-all_test = data_set_test[:,1:9]
+##### dividing into training set and test set #####
+# raw data files
+result_train = open('./dataset/train_raw.csv', 'w', encoding="utf-8")
+result_pred = open('./dataset/test_raw.csv', 'w', encoding="utf-8")
+result_pred_all = open('./dataset/test_raw_all.csv', 'w', encoding="utf-8")
 
-# define variable types
-all_train = all_train.astype('float32')
-all_test = all_test.astype('float32')
+# log scale conversion files
+result_train_log = open('./dataset/train_log.csv', 'w', encoding="utf-8")
+result_pred_log = open('./dataset/test_log.csv', 'w', encoding="utf-8")
+result_pred_all_log = open('./dataset/test_log_all.csv', 'w', encoding="utf-8")
 
-##### 3. normalization #####
-scaler = StandardScaler()
-scaler.fit(all_train)
-scaled_all_train = scaler.transform(all_train)
-scaled_x_train = scaled_all_train[:,1:8]	# seven input parameters
-scaled_y_train = scaled_all_train[:,0]		# target parameter (FIMS observation)
-
-# application of nomalization to the test set
-scaled_all_test = scaler.transform(all_test)
-scaled_x_test = scaled_all_test[:,1:8]		# seven input parameters
-
-##### 4. modeling #####
-# three layers: tanh + relu + relu
-model = Sequential()
-model.add(Dense(16, kernel_initializer='RandomUniform', activation='tanh', input_shape=(7,)))
-model.add(Dropout(0.03))
-model.add(Dense(16, kernel_initializer='uniform', activation='relu'))
-model.add(Dropout(0.03))
-model.add(Dense(16, activation='relu'))
-model.add(Dropout(0.05))
-model.add(Dense(1))
-
-# model compile: loass function = mean squared error
-model.compile(loss='mean_squared_error', optimizer=RMSprop(lr=0.0001), metrics=[metrics.mae])
-
-# training
-history = model.fit(scaled_x_train, scaled_y_train, epochs=1000)
-
-##### 5. plottng result #####
-# accuracy of training
-plt.figure(1)
-plt.title('training accuracy')
-plt.plot(history.history['mean_absolute_error'])
-plt.xlabel("Epoch")
-plt.ylabel("Loss")
-plt.show()
-
-##### 6. inverse transformation #####
-scaled_y_pred = model.predict(scaled_x_test)
-scaled_all_test[:,0] = list(scaled_y_pred.flatten())
-all_pred = scaler.inverse_transform(scaled_all_test)
-y_pred = all_pred[:,0]
-y_pred = list(y_pred.flatten())
-
-##### 7. converting log-scale into continuum unit #####
-y_pred = np.exp(y_pred)/1000
-
-##### 8. saving result #####
-result = open('./dataset/result.csv', 'w', encoding="utf-8")
+##### dividing data sets and saving #####
 # data order: pixel_number + fims_observation + fims_prediction + gal_lon + gal_lat + rass1 + rass2 + H_alpha + E(B-V) + N(HI) + galex_oservation
-for i in range(len(y_pred)):
-	result.write(str(data_set_test[i,0]) +',' +str(data_set_test_raw[i,1]) +',' +str(y_pred[i]) + ',' + str(data_set_test[i,2]) + ',' + str(data_set_test[i,3])+ ',' + str(data_set_test_raw[i,4]) +
-	',' + str(data_set_test_raw[i,5]) + ',' + str(data_set_test_raw[i,6])+ ',' + str(data_set_test_raw[i,7]) + ',' + str(data_set_test_raw[i,8]) +','  + str(data_set_test[i,9]) + '\n')
-result.close()
+for i in range(len(non_fims)):
+
+	if str(non_rass1[i]) != 'nan' and str(non_rass2[i]) != 'nan' and str(non_ha[i]) != 'nan' and str(non_ebv[i]) != 'nan' and str(non_hi[i]) != 'nan' :
+
+		# entire data set (raw)
+		result_pred_all.write(str(index[i]) +',' +str(non_fims[i]) + ',' + str(l[i]) + ',' + str(b[i])+ ',' + str(non_rass1[i]) +
+		',' + str(non_rass2[i]) + ',' + str(non_ha[i])+ ',' + str(non_ebv[i]) + ',' + str(non_hi[i]) +','  + str(non_galex[i]) + '\n')
+
+		# test set
+		if str(non_fims[i]) == 'nan':
+			result_pred.write(str(index[i]) +',' +str(non_fims[i]) + ',' + str(l[i]) + ',' + str(b[i])+ ',' + str(non_rass1[i]) +
+			',' + str(non_rass2[i]) + ',' + str(non_ha[i])+ ',' + str(non_ebv[i]) + ',' + str(non_hi[i]) +','  + str(non_galex[i]) + '\n')
+			result_pred_log.write(str(index[i]) +',' +str(non_fims[i]) + ',' + str(l[i]) + ',' + str(b[i])+ ',' + str(np.log(10*non_rass1[i])) +
+			',' + str(np.log(10*non_rass2[i])) + ',' + str(np.log(10000*non_ha[i]))+ ',' + str(np.log(100000*non_ebv[i])) + ',' + str(np.log(non_hi[i]/1e16)) +','  + str(non_galex[i]) + '\n')
+			# entire data set (log)
+			result_pred_all_log.write(str(index[i]) +',' +str(non_fims[i]) + ',' + str(l[i]) + ',' + str(b[i])+ ',' + str(np.log(10*non_rass1[i])) +
+			',' + str(np.log(10*non_rass2[i])) + ',' + str(np.log(10000*non_ha[i]))+ ',' + str(np.log(100000*non_ebv[i])) + ',' + str(np.log(non_hi[i]/1e16)) +','  + str(non_galex[i]) + '\n')
+		# training set
+		else:
+			result_train.write(str(index[i]) +',' +str(non_fims[i]) + ',' + str(l[i]) + ',' + str(b[i])+ ',' + str(non_rass1[i]) +
+			',' + str(non_rass2[i]) + ',' + str(non_ha[i])+ ',' + str(non_ebv[i]) + ',' + str(non_hi[i]) +','  + str(non_galex[i]) + '\n')
+			result_train_log.write(str(index[i]) +',' +str(np.log(1000*non_fims[i])) + ',' + str(l[i]) + ',' + str(b[i])+ ',' + str(np.log(10*non_rass1[i])) +
+			',' + str(np.log(10*non_rass2[i])) + ',' + str(np.log(10000*non_ha[i]))+ ',' + str(np.log(100000*non_ebv[i])) + ',' + str(np.log(non_hi[i]/1e16)) +','  + str(non_galex[i]) + '\n')
+			# entire data set (log)
+			result_pred_all_log.write(str(index[i]) +',' +str(np.log(1000*non_fims[i])) + ',' + str(l[i]) + ',' + str(b[i])+ ',' + str(np.log(10*non_rass1[i])) +
+			',' + str(np.log(10*non_rass2[i])) + ',' + str(np.log(10000*non_ha[i]))+ ',' + str(np.log(100000*non_ebv[i])) + ',' + str(np.log(non_hi[i]/1e16)) +','  + str(non_galex[i]) + '\n')
+
+# flie closing
+result_pred_all_log.close()
+result_pred_log.close()
+result_train_log.close()
+
+result_pred_all.close()
+result_pred.close()
+result_train.close()

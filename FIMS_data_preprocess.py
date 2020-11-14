a54ddc8374ec830b9667c82fbdaac6d63da4991f
yjchoi83 <choiyj83@gmail.com>
2020-11-14 13:16:55 +0900

    Update FIMS_data_preprocess.py

--- FIMS_data_preprocess.py
+++ FIMS_data_preprocess.py
@@ -4,121 +4,103 @@ import pandas as pd
 import sys,csv
 import datetime
 
-# temp_a = pd.read_csv('./dataset/test_log_final.csv',header = None)
-# temp_a = pd.DataFrame(temp_a)
-# temp_a = temp_a.values[0:]
-#
-# temp_b = pd.read_csv('./dataset/temp_com.csv',header = None)
-# temp_b = pd.DataFrame(temp_b)
-# temp_b = temp_b.values[0:]
-#
-# a = temp_a[:,0]
-# b = temp_b[:,0:3]
-#
-# result = open('./dataset/result/temp.csv', 'w', encoding="utf-8")
-#
-# for i in range(len(a)):
-#     for j in range(len(b)):
-#         if str(a[i]) == str(b[j]):
-#             result.write(str(b[j,0]) + ','+ str(b[j,1]) + ',' + str(b[j,2]) + '\n')
-#
-#         else:
-#             continue
-#
-# result.close()
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import Normalizer
+from sklearn.preprocessing import MaxAbsScaler
 
-data_set_train = pd.read_csv('./dataset/allskymap_raw.csv',header=None)
+from keras.layers import Dense, Dropout, Activation,LeakyReLU,PReLU
+from sklearn import preprocessing
+from keras.models import Sequential
+from keras.optimizers import SGD,Adam,RMSprop
+from keras.utils import np_utils
+from keras import initializers
+
+from scipy.optimize import curve_fit
+from keras import metrics
+from sklearn.model_selection import train_test_split
+
+import matplotlib.pyplot as plt
+from math import  log
+
+dt = datetime.datetime.now()
+dt = dt.strftime("%m%d%H%M%S")
+
+##### 1. loading files #####
+# training data loading
+data_set_train = pd.read_csv('./dataset/train_log.csv',header=None)
 data_set_train = pd.DataFrame(data_set_train)
-data_set_train = data_set_train.values[1:]
+data_set_train = data_set_train.values[:]
 
-index = data_set_train[:,0]
-l = data_set_train[:,1]
-b = data_set_train[:,2]
-non_fims = data_set_train[:,3]
-non_galex =data_set_train[:,4]
-non_rass1 =data_set_train[:,5]
-non_rass2 = data_set_train[:,6]
-non_ha = data_set_train[:,7]
-non_euv = data_set_train[:,8]
-non_hi = data_set_train[:,9]
-#
-#
-#
-#result_train = open('./dataset/train_raw.csv', 'w', encoding="utf-8")
-result_pred = open('./dataset/test_raw_new.csv', 'w', encoding="utf-8")
-# for i in range(len(non_fims)):
-#
-#     if str(non_fims[i]) == 'nan' :
-#         continue
-#     else:
-#         result_train.write(str(index[i])+','+str(non_fims[i]) + ',' + str(non_galex[i])+',' + str(non_rass1[i]) +
-#         ',' + str(non_rass2[i])+',' + str(non_ha[i])+',' + str(non_euv[i])+',' + str(non_hi[i])+ '\n')
-#
-# #
-for i in range(len(non_fims)):
+# test data loading (log scale)
+data_set_test = pd.read_csv('./dataset/test_log_all.csv',header=None)
+data_set_test = pd.DataFrame(data_set_test)
+data_set_test = data_set_test.values[:]
 
-    # if str(non_fims[i]) == 'nan' :
-    #     result_pred.write(str(index[i]) +',' + str(non_fims[i])+ ',' + str(non_galex[i]) + ',' + str(non_rass1[i]) +
-    #                           ',' + str(non_rass2[i]) + ',' + str(non_ha[i]) + ',' + str(non_euv[i]) + ',' + str(
-    #             non_hi[i]) + '\n')
+# test data loading (raw)
+data_set_test_raw = pd.read_csv('./dataset/test_raw_all.csv',header=None)
+data_set_test_raw = pd.DataFrame(data_set_test_raw)
+data_set_test_raw = data_set_test_raw.values[:]
 
-    if str(non_fims[i]) == 'nan' :
-        result_pred.write(str(index[i]) +',' +str(non_fims[i]) + ',' + str(l[i]) + ',' + str(b[i])+ ',' + str(non_rass1[i]) +
-                              ',' + str(non_rass2[i]) + ',' + str(non_ha[i])+ ',' + str(non_euv[i]) + ',' + str(
-                non_hi[i]) +','  + str(non_galex[i]) + '\n')
+##### 2. data set #####
+all_train = data_set_train[:,1:9]
+all_test = data_set_test[:,1:9]
 
+# define variable types
+all_train = all_train.astype('float32')
+all_test = all_test.astype('float32')
 
-result_pred.close()
-# result_pred.close()
+##### 3. normalization #####
+scaler = StandardScaler()
+scaler.fit(all_train)
+scaled_all_train = scaler.transform(all_train)
+scaled_x_train = scaled_all_train[:,1:8]	# seven input parameters
+scaled_y_train = scaled_all_train[:,0]		# target parameter (FIMS observation)
 
-#
-# data_set_train = pd.read_csv('./dataset/allskymap_raw.csv',header=None)
-# data_set_train = pd.DataFrame(data_set_train)
-# data_set_train = data_set_train.values[1:]
-#
-# test_galex = open('./dataset/test_final.csv', 'w', encoding="utf-8")
-#
-# temp = pd.read_csv('./dataset/test_raw.csv',header=None)
-# temp = pd.DataFrame(temp)
-# temp = temp.values[0:]
-#
-# index = temp[:,0]
-# non_fims = temp[:,1]
-# non_galex =temp[:,2]
-# non_rass1 =temp[:,3]
-# non_rass2 = temp[:,4]
-# non_ha = temp[:,5]
-# non_euv = temp[:,6]
-# non_hi = temp[:,7]
-#
-# for i in range(len(non_galex)):
-#
-#     if str(non_galex[i]) == 'nan' :
-#         continue
-#
-#     else:
-#         test_galex.write(str(index[i]) +',' + str(non_fims[i])+ ',' + str(non_galex[i]) + ',' + str(non_rass1[i]) +
-#             ',' + str(non_rass2[i]) + ',' + str(non_ha[i]) + ',' + str(non_euv[i]) + ',' + str(
-#                 non_hi[i]) + '\n')
-#
-# test_galex.close()
+# application of nomalization to the test set
+scaled_all_test = scaler.transform(all_test)
+scaled_x_test = scaled_all_test[:,1:8]		# seven input parameters
 
+##### 4. modeling #####
+# three layers: tanh + relu + relu
+model = Sequential()
+model.add(Dense(16, kernel_initializer='RandomUniform', activation='tanh', input_shape=(7,)))
+model.add(Dropout(0.03))
+model.add(Dense(16, kernel_initializer='uniform', activation='relu'))
+model.add(Dropout(0.03))
+model.add(Dense(16, activation='relu'))
+model.add(Dropout(0.05))
+model.add(Dense(1))
 
-#
-# union = open('./dataset/f_g_union.csv', 'w', encoding="utf-8")
-#
-# temp = pd.read_csv('./dataset/fims_galex_union.csv',header=None)
-# temp = pd.DataFrame(temp)
-# temp = temp.values[0:]
-#
-#
-# non_fims = temp[:,0]
-# non_galex =temp[:,1]
-#
-# for i in range(len(non_galex)):
-#
-#     if str(non_fims[i]) != 'nan' and str(non_galex[i]) != 'nan':
-#         union.write(str(non_fims[i]) + ','+ str( non_galex[i]) + '\n')
-#
-# union.close()
+# model compile: loass function = mean squared error
+model.compile(loss='mean_squared_error', optimizer=RMSprop(lr=0.0001), metrics=[metrics.mae])
 
+# training
+history = model.fit(scaled_x_train, scaled_y_train, epochs=1000)
+
+##### 5. plottng result #####
+# accuracy of training
+plt.figure(1)
+plt.title('training accuracy')
+plt.plot(history.history['mean_absolute_error'])
+plt.xlabel("Epoch")
+plt.ylabel("Loss")
+plt.show()
+
+##### 6. inverse transformation #####
+scaled_y_pred = model.predict(scaled_x_test)
+scaled_all_test[:,0] = list(scaled_y_pred.flatten())
+all_pred = scaler.inverse_transform(scaled_all_test)
+y_pred = all_pred[:,0]
+y_pred = list(y_pred.flatten())
+
+##### 7. converting log-scale into continuum unit #####
+y_pred = np.exp(y_pred)/1000
+
+##### 8. saving result #####
+result = open('./dataset/result.csv', 'w', encoding="utf-8")
+# data order: pixel_number + fims_observation + fims_prediction + gal_lon + gal_lat + rass1 + rass2 + H_alpha + E(B-V) + N(HI) + galex_oservation
+for i in range(len(y_pred)):
+	result.write(str(data_set_test[i,0]) +',' +str(data_set_test_raw[i,1]) +',' +str(y_pred[i]) + ',' + str(data_set_test[i,2]) + ',' + str(data_set_test[i,3])+ ',' + str(data_set_test_raw[i,4]) +
+	',' + str(data_set_test_raw[i,5]) + ',' + str(data_set_test_raw[i,6])+ ',' + str(data_set_test_raw[i,7]) + ',' + str(data_set_test_raw[i,8]) +','  + str(data_set_test[i,9]) + '\n')
+result.close()
